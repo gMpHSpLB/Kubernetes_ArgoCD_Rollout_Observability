cf8aa6501a04b7a21847c4253980c2abd1242169
hcrmclass <hcrmclass@gmail.com>
2026-03-29 18:56:08 +0000

    Fix CI DB connection

--- db_test.py
+++ db_test.py
@@ -1,10 +1,12 @@
+import os
 import psycopg2
 
 conn = psycopg2.connect(
-    host="localhost",
+    host=os.getenv("DB_HOST", "localhost"),
     database="mydb",
     user="myuser",
-    password="mypassword"
+    password="mypassword",
+    port=5432
 )
 
 print("Connected!")
